48d146d5182399ec3a1b6a94b08711448b6a74f2
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2026-02-06 19:51:46 +0800

    better buttons

--- src/Control.tsx
+++ src/Control.tsx
@@ -3,6 +3,7 @@ import { Button } from "./components/ui/button";
 import { useGame } from "./pixi/Game/use-game";
 import { transition } from "./utils/game-transition";
 import { Key } from "./pixi/Input";
+import { useEffect } from "react";
 
 const label = {
 	pause: "Start",
@@ -11,9 +12,9 @@ const label = {
 } as const;
 
 export function Control() {
-	const { game, input } = useGame();
+	const { game } = useGame();
 	return (
-		<div id="control" className="flex flex-col gap-2 justify-between px-1">
+		<div id="control" className="justify-between px-1 flex flex-col gap-2">
 			<div className="flex justify-between items-center gap-1">
 				<div className="flex  gap-2 items-center">
 					<div>Score {game.score}</div>
@@ -21,58 +22,86 @@ export function Control() {
 				</div>
 				<Button onClick={transition[game.status]}>{label[game.status]}</Button>
 			</div>
-			<div className="flex justify-center gap-2 items-center">
+			<Arrow></Arrow>
+		</div>
+	);
+}
+
+function Arrow() {
+	const { input } = useGame();
+	useEffect(() => {
+		function resize() {
+			const root = document.getElementById("root") as HTMLDivElement;
+			const width = window.innerWidth;
+			const height = window.innerHeight - root.clientHeight;
+			const size = Math.min((width - 16) / 3, (height - 62) / 2);
+			console.log("window", window.innerHeight);
+			console.log("root", root.clientHeight);
+			console.log({ width, height, size });
+			const els = document.querySelectorAll<HTMLElement>(".btn");
+			els.forEach((el) => {
+				el.style.setProperty("width", size + "px");
+				el.style.setProperty("height", size + "px");
+			});
+		}
+		resize();
+		window.addEventListener("resize", resize);
+		return () => {
+			window.removeEventListener("resize", resize);
+		};
+	}, []);
+	return (
+		<div id="arrow-btns" className="flex opacity-60 flex-1 justify-center gap-2 items-center">
+			<Button
+				onClick={() => {
+					input.push(Key.Left);
+					setTimeout(() => {
+						input.press = null;
+					}, 200);
+				}}
+				className="btn rounded-l-full"
+				variant={input.press === Key.Left ? "default" : "secondary"}
+			>
+				<ChevronLeft className="w-full h-full aspect-square" />
+			</Button>
+			<div className="flex flex-col gap-2">
 				<Button
 					onClick={() => {
-						input.push(Key.Left);
+						input.push(Key.Up);
 						setTimeout(() => {
 							input.press = null;
 						}, 200);
 					}}
-					className="w-12 h-12"
-					variant={input.press === Key.Left ? "default" : "secondary"}
+					className="btn rounded-t-full"
+					variant={input.press === Key.Up ? "default" : "secondary"}
 				>
-					<ChevronLeft />
+					<ChevronUp className="w-full h-full aspect-square" />
 				</Button>
-				<div className="flex flex-col gap-2">
-					<Button
-						onClick={() => {
-							input.push(Key.Up);
-							setTimeout(() => {
-								input.press = null;
-							}, 200);
-						}}
-						className="h-12 w-12"
-						variant={input.press === Key.Up ? "default" : "secondary"}
-					>
-						<ChevronUp />
-					</Button>
-					<Button
-						onClick={() => {
-							input.push(Key.Down);
-							setTimeout(() => {
-								input.press = null;
-							}, 200);
-						}}
-						className="h-12 w-12"
-						variant={input.press === Key.Down ? "default" : "secondary"}
-					>
-						<ChevronDown />
-					</Button>
-				</div>
 				<Button
 					onClick={() => {
-						input.push(Key.Right);
+						input.push(Key.Down);
 						setTimeout(() => {
 							input.press = null;
 						}, 200);
 					}}
-					className="w-12 h-12"
-					variant={input.press === Key.Right ? "default" : "secondary"}
+					className="btn rounded-b-full"
+					variant={input.press === Key.Down ? "default" : "secondary"}
 				>
-					<ChevronRight />
+					<ChevronDown className="w-full h-full aspect-square" />
 				</Button>
 			</div>
+			<Button
+				onClick={() => {
+					input.push(Key.Right);
+					setTimeout(() => {
+						input.press = null;
+					}, 200);
+				}}
+				className="btn rounded-r-full "
+				variant={input.press === Key.Right ? "default" : "secondary"}
+			>
+				<ChevronRight className="w-full h-full aspect-square" />
+			</Button>
 		</div>
 	);
 }
